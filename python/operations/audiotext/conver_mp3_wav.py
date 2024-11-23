1dbadf08ee7d0096fab5e6720e686ac8a5001dbb
SasakinME <max_to_day@mail.ru>
2024-11-23 14:56:07 +0300

    add transcrib audio to text

--- python/operations/audiotext/conver_mp3_wav.py
+++ python/operations/audiotext/conver_mp3_wav.py
@@ -1,3 +1,5 @@
+import os
+from pydub import AudioSegment
 
 # Функция для преобразования MP3 в WAV
 def convert_mp3_to_wav(mp3_file):
@@ -12,11 +14,9 @@ def main(mp3_file):
     if not os.path.exists(mp3_file):
        print("Файл не найден!")
        return
-
-
-    print(text)
+    convert_mp3_to_wav(mp3_file)
 
 # Пример использования
 if __name__ == "__main__":
-    mp3_file_path = "video.mp3"  # Укажите путь к вашему MP3 файлу
+    mp3_file_path = "input/video.mp3"  # Укажите путь к вашему MP3 файлу
     main(mp3_file_path)
