dd080f135a3543fb91c191ca4ccc10f287a6afc0
SasakinME <max_to_day@mail.ru>
2024-12-07 15:11:34 +0300

    add cursor plugin

--- python/operations/getcourse/parse_html.py
+++ python/operations/getcourse/parse_html.py
@@ -1,10 +1,20 @@
 import requests
 
-# URL страницы, которую вы хотите скопировать
-url = 'https://schoolterebenin.getcourse.ru/cms/system/login'  # Замените на нужный URL
+url = input('Введите URL сайта (например, https://.../): ')
+
+# Настройка заголовков для имитации браузера
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+}
 
 # Получение содержимого страницы
-response = requests.get(url, verify=False)
+response = requests.get(
+        url,
+        headers=headers,
+        verify=False,
+        timeout=10
+    )
 
 # Проверка успешности запроса
 if response.status_code == 200:
